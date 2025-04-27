1f615150f95217dd9729c212a2aab45a279cb09c
Vinod <21vinod12@gmail.com>
2025-04-27 18:21:01 -0400

    link to download my resume

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Resume from './components/Resume';
 
 function App() {
   return (
     <div className="App">
       <h1> Hello World!!</h1>
-      <p> This page is going to be my resume soon..</p>
+      <p> This page is going to be my resume soon..
+        Check the Text version for now. <Resume/>
+      </p>
     </div>
   );
 }
---
+++ src/components/Resume.js
@@ -0,0 +1,17 @@
+import react from 'react';
+import { useState } from 'react';
+
+function Resume() {
+
+    return (
+        <div className="resume">
+
+            <a href={require('../files/Vinod_Mitta_Resume_Sr_QA.pdf')} target="_blank" rel="noopener noreferrer"> Download Resume</a>
+
+
+        </div>
+    );
+
+}
+
+export default Resume;
