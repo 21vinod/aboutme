dd30a60dc9d1efd0948e764a28a07d3e463c4e82
Vinod <21vinod12@gmail.com>
2025-04-27 17:36:42 -0400

    create-react-app and modifications to host on github

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello World!!</h1>
+      <p> This page is going to be my resume soon..</p>
+    </div>
+  );
+}
+
+export default App;
